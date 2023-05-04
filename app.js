3d171e26a007b5550383c4a1ff2b53174598dbfe
MasterZorigh <73075421+MasterZorigh@users.noreply.github.com>
2023-05-04 22:35:17 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var cors = require("cors");
+
+var indexRouter = require("./routes/index");
+var bandRouter = require("./routes/band");
+
+var app = express();
+
+// Intégration de la bdd
+var connectionString =
+  "mongodb+srv://MasterZorigh:ZoKoTeRigh2713*@find-my-band.nsgkfcv.mongodb.net/?retryWrites=true&w=majority";
+var mongoDB = process.env.MONGODB_URI || connectionString;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
+
+app.use("/", indexRouter);
+app.use("/band", bandRouter);
+
+module.exports = app;
